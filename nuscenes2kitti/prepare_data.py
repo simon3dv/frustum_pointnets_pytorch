8b2daf83ebc7407c0984c02d28f41354b45ba06a
simon3dv <gzfansiming@gmail.com>
2020-01-26 12:02:49 +0800

    update nuscenes2kitti

--- nuscenes2kitti/prepare_data.py
+++ nuscenes2kitti/prepare_data.py
@@ -177,8 +177,11 @@ def demo():
     print(calib)
     # Draw lidar in rect camera coord
     print(' -------- LiDAR points in rect camera coordination --------')
-    pc_rect = calib.project_velo_to_rect(pc_velo)
-    fig = draw_lidar_simple(pc_rect)
+    print('pc_velo.shape:',pc_velo.shape)
+    view = np.eye(4)
+    pc_velo = utils.view_points(pc_velo[:3, :], view, normalize=False)
+    # pc_rect = calib.project_velo_to_rect(pc_velo)
+    fig = draw_lidar_simple(pc_velo)
     raw_input()
 
     # Draw 2d and 3d boxes on image
