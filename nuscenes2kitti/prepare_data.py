83a94e52f687828e2a764146301e1f87064d081a
simon3dv <gzfansiming@gmail.com>
2020-01-28 19:32:19 +0800

    update nuscenes2kitti

--- nuscenes2kitti/prepare_data.py
+++ nuscenes2kitti/prepare_data.py
@@ -266,9 +266,8 @@ def demo(data_idx):
         if obj.type=='DontCare':continue
         # Draw 3d bounding box
         box3d_pts_2d, box3d_pts_3d = utils.compute_box_3d(obj, np.eye(4))#(8,2),(8,3)
-        box3d_pts_3d_velo = calib.project_global_to_velo(box3d_pts_3d.T)#(3,8)
-        #box3d_pts_3d_ego = calib.project_global_to_ego(box3d_pts_3d)
-        #box3d_pts_3d_velo = calib.project_ego_to_velo(box3d_pts_3d)
+        box3d_pts_3d_global = calib.project_cam_to_global(box3d_pts_3d.T)  # (3,8)
+        box3d_pts_3d_velo = calib.project_global_to_velo(box3d_pts_3d_global)#(3,8)
         corners = box3d_pts_3d_velo#(3,8)
         def draw_rect(selected_corners, color):
             prev = selected_corners[-1]
