21037a4981b0ef79614b780f5edf03e5b544950c
simon3dv <gzfansiming@gmail.com>
2020-01-28 16:44:19 +0800

    update nuscenes2kitti

--- nuscenes2kitti/nuscenes2kitti_object.py
+++ nuscenes2kitti/nuscenes2kitti_object.py
@@ -277,7 +277,6 @@ def show_lidar_with_boxes(pc_velo, objects, calib, sensor,
     for obj in objects:
         if obj.type=='DontCare':continue
         # Draw 3d bounding box
-        ipdb.set_trace()
         box3d_pts_2d, box3d_pts_3d = utils.compute_box_3d(obj, np.eye(4))#(8,2),(8,3)
         box3d_pts_3d_global = calib.project_cam_to_global(box3d_pts_3d.T, sensor)  # (3,8)
         box3d_pts_3d_velo = calib.project_global_to_velo(box3d_pts_3d_global).T#(8,3)
@@ -285,7 +284,7 @@ def show_lidar_with_boxes(pc_velo, objects, calib, sensor,
         # Draw heading arrow
         ori3d_pts_2d, ori3d_pts_3d = utils.compute_orientation_3d(obj, np.eye(4))#(2,2),(2,3)
         ori3d_pts_3d_global = calib.project_cam_to_global(ori3d_pts_3d.T, sensor)#(3,2)
-        ori3d_pts_3d_velo = calib.project_global_to_velo(ori3d_pts_3d_global.T).T#(2,3)
+        ori3d_pts_3d_velo = calib.project_global_to_velo(ori3d_pts_3d_global).T#(2,3)
         ori3d_pts_3d_velo = ori3d_pts_3d
         x1,y1,z1 = ori3d_pts_3d_velo[0,:]
         x2,y2,z2 = ori3d_pts_3d_velo[1,:]
