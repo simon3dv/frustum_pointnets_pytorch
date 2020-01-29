10e744fac289ff3c0e7307bdd7ec72fb5f94027e
simon3dv <gzfansiming@gmail.com>
2020-01-30 01:55:18 +0800

    update train and test

--- train/test.py
+++ train/test.py
@@ -60,7 +60,8 @@ if FLAGS.dataset == 'kitti':
         overwritten_data_path = FLAGS.data_path
     TEST_DATASET = provider.FrustumDataset(npoints=NUM_POINT, split='val',
                                            rotate_to_center=True, one_hot=True,
-                                           overwritten_data_path=overwritten_data_path)
+                                           overwritten_data_path=overwritten_data_path,
+                                           from_rgb_detection=FLAGS.from_rgb_detection)
 elif FLAGS.dataset == 'nuscenes2kitti':
     SENSOR = FLAGS.sensor
     overwritten_data_path_prefix = 'nuscenes2kitti/frustum_' + FLAGS.objtype + '_' + SENSOR + '_'
@@ -70,7 +71,8 @@ elif FLAGS.dataset == 'nuscenes2kitti':
         overwritten_data_path = FLAGS.data_path
     TEST_DATASET = provider.FrustumDataset(npoints=NUM_POINT, split='val',
                                            rotate_to_center=True, one_hot=True,
-                                           overwritten_data_path=overwritten_data_path)
+                                           overwritten_data_path=overwritten_data_path,
+                                           from_rgb_detection=FLAGS.from_rgb_detection)
 else:
     print('Unknown dataset: %s' % (FLAGS.dataset))
     exit(-1)
@@ -307,7 +309,6 @@ def test_one_epoch(model, loader):
         batch_seg_prob = softmax(logits)[:, :, 1]  # (32, 1024, 2) ->(32, 1024)
         batch_seg_mask = np.argmax(logits, 2)  # BxN
         mask_mean_prob = np.sum(batch_seg_prob * batch_seg_mask, 1)  # B,
-        #ipdb.set_trace()
         mask_mean_prob = mask_mean_prob / np.sum(batch_seg_mask, 1)  # B,
         heading_prob = np.max(softmax(heading_scores), 1)  # B
         size_prob = np.max(softmax(size_scores), 1)  # B,
@@ -383,6 +384,256 @@ def test_one_epoch(model, loader):
                test_acc/test_n_samples, \
                test_iou3d_acc/test_n_samples
 
+def test_from_rgb_detection(model, loader):
+    ''' Test frustum pointnets with GT 2D boxes.
+    Write test results to KITTI format label files.
+    todo (rqi): support variable number of points.
+    '''
+    ps_list = []
+    seg_list = []
+    segp_list = []
+    center_list = []
+    heading_cls_list = []
+    heading_res_list = []
+    size_cls_list = []
+    size_res_list = []
+    rot_angle_list = []
+    score_list = []
+    onehot_list = []
+
+    test_idxs = np.arange(0, len(TEST_DATASET))
+    batch_size = BATCH_SIZE
+    num_batches = len(TEST_DATASET) // batch_size
+
+    test_n_samples = 0
+    test_total_loss = 0.0
+    test_iou2d = 0.0
+    test_iou3d = 0.0
+    test_acc = 0.0
+    test_iou3d_acc = 0.0
+    eval_time = 0.0
+    if FLAGS.return_all_loss:
+        test_mask_loss = 0.0
+        test_center_loss = 0.0
+        test_heading_class_loss = 0.0
+        test_size_class_loss = 0.0
+        test_heading_residuals_normalized_loss = 0.0
+        test_size_residuals_normalized_loss = 0.0
+        test_stage1_center_loss = 0.0
+        test_corners_loss = 0.0
+
+    for i, data in tqdm(enumerate(loader), \
+                        total=len(loader), smoothing=0.9):
+        # for debug
+        if FLAGS.debug == True:
+            if i == 1:
+                break
+        test_n_samples += data[0].shape[0]
+        '''
+        batch_data:[32, 2048, 4], pts in frustum
+        batch_label:[32, 2048], pts ins seg label in frustum
+        batch_center:[32, 3],
+        batch_hclass:[32],
+        batch_hres:[32],
+        batch_sclass:[32],
+        batch_sres:[32,3],
+        batch_rot_angle:[32],
+        batch_one_hot_vec:[32,3],
+        '''
+        # 1. Load data
+        batch_data, batch_label, batch_center, \
+        batch_hclass, batch_hres, \
+        batch_sclass, batch_sres, \
+        batch_rot_angle, batch_one_hot_vec = data
+
+        batch_data = batch_data.transpose(2, 1).float().cuda()
+        batch_label = batch_label.float().cuda()
+        batch_center = batch_center.float().cuda()
+        batch_hclass = batch_hclass.float().cuda()
+        batch_hres = batch_hres.float().cuda()
+        batch_sclass = batch_sclass.float().cuda()
+        batch_sres = batch_sres.float().cuda()
+        batch_rot_angle = batch_rot_angle.float().cuda()
+        batch_one_hot_vec = batch_one_hot_vec.float().cuda()
+
+        # 2. Eval one batch
+        model = model.eval()
+        eval_t1 = time.perf_counter()
+        logits, mask, stage1_center, center_boxnet, \
+        heading_scores, heading_residuals_normalized, heading_residuals, \
+        size_scores, size_residuals_normalized, size_residuals, center = \
+            model(batch_data, batch_one_hot_vec)
+        # logits:[32, 1024, 2] , mask:[32, 1024]
+        eval_t2 = time.perf_counter()
+        eval_time += (eval_t2 - eval_t1)
+
+        # 3. Compute Loss
+        if FLAGS.return_all_loss:
+            total_loss, mask_loss, center_loss, heading_class_loss, \
+            size_class_loss, heading_residuals_normalized_loss, \
+            size_residuals_normalized_loss, stage1_center_loss, \
+            corners_loss = \
+                Loss(logits, batch_label, \
+                     center, batch_center, stage1_center, \
+                     heading_scores, heading_residuals_normalized, \
+                     heading_residuals, \
+                     batch_hclass, batch_hres, \
+                     size_scores, size_residuals_normalized, \
+                     size_residuals, \
+                     batch_sclass, batch_sres)
+        else:
+            total_loss = \
+                Loss(logits, batch_label, \
+                     center, batch_center, stage1_center, \
+                     heading_scores, heading_residuals_normalized, \
+                     heading_residuals, \
+                     batch_hclass, batch_hres, \
+                     size_scores, size_residuals_normalized, \
+                     size_residuals, \
+                     batch_sclass, batch_sres)
+
+        test_total_loss += total_loss.item()
+        if FLAGS.return_all_loss:
+            test_mask_loss += mask_loss.item()
+            test_center_loss += center_loss.item()
+            test_heading_class_loss += heading_class_loss.item()
+            test_size_class_loss += size_class_loss.item()
+            test_heading_residuals_normalized_loss += heading_residuals_normalized_loss.item()
+            test_size_residuals_normalized_loss += size_residuals_normalized_loss.item()
+            test_stage1_center_loss += stage1_center_loss.item()
+            test_corners_loss += corners_loss.item()
+
+        # 4. compute seg acc, IoU and acc(IoU)
+        correct = torch.argmax(logits, 2).eq(batch_label.detach().long()).cpu().numpy()
+        accuracy = np.sum(correct) / float(NUM_POINT)
+        test_acc += accuracy
+
+        logits = logits.cpu().detach().numpy()
+        mask = mask.cpu().detach().numpy()
+        center_boxnet = center_boxnet.cpu().detach().numpy()
+        # stage1_center = stage1_center.cpu().detach().numpy()#
+        center = center.cpu().detach().numpy()
+        heading_scores = heading_scores.cpu().detach().numpy()
+        # heading_residuals_normalized = heading_residuals_normalized.cpu().detach().numpy()
+        heading_residuals = heading_residuals.cpu().detach().numpy()
+        size_scores = size_scores.cpu().detach().numpy()
+        size_residuals = size_residuals.cpu().detach().numpy()
+        # size_residuals_normalized = size_residuals_normalized.cpu().detach().numpy()#
+
+        batch_center = batch_center.cpu().detach().numpy()
+        batch_hclass = batch_hclass.cpu().detach().numpy()
+        batch_hres = batch_hres.cpu().detach().numpy()
+        batch_sclass = batch_sclass.cpu().detach().numpy()
+        batch_sres = batch_sres.cpu().detach().numpy()
+
+        iou2ds, iou3ds = provider.compute_box3d_iou(
+            center,
+            heading_scores,
+            heading_residuals,
+            size_scores,
+            size_residuals,
+            batch_center,
+            batch_hclass,
+            batch_hres,
+            batch_sclass,
+            batch_sres)
+        test_iou2d += np.sum(iou2ds)
+        test_iou3d += np.sum(iou3ds)
+        test_iou3d_acc += np.sum(iou3ds >= 0.7)
+
+        # 5. Compute and write all Results
+        batch_output = np.argmax(logits, 2)  # mask#torch.Size([32, 1024])
+        batch_center_pred = center  # _boxnet#torch.Size([32, 3])
+        batch_hclass_pred = np.argmax(heading_scores, 1)  # (32,)
+        batch_hres_pred = np.array([heading_residuals[j, batch_hclass_pred[j]] \
+                                    for j in range(batch_data.shape[0])])  # (32,)
+        # batch_size_cls,batch_size_res
+        batch_sclass_pred = np.argmax(size_scores, 1)  # (32,)
+        batch_sres_pred = np.vstack([size_residuals[j, batch_sclass_pred[j], :] \
+                                     for j in range(batch_data.shape[0])])  # (32,3)
+
+        # batch_scores
+        batch_seg_prob = softmax(logits)[:, :, 1]  # (32, 1024, 2) ->(32, 1024)
+        batch_seg_mask = np.argmax(logits, 2)  # BxN
+        mask_mean_prob = np.sum(batch_seg_prob * batch_seg_mask, 1)  # B,
+        mask_mean_prob = mask_mean_prob / np.sum(batch_seg_mask, 1)  # B,
+        heading_prob = np.max(softmax(heading_scores), 1)  # B
+        size_prob = np.max(softmax(size_scores), 1)  # B,
+        batch_scores = np.log(mask_mean_prob) + np.log(heading_prob) + np.log(size_prob)
+
+        for j in range(batch_output.shape[0]):
+            ps_list.append(batch_data[j, ...])
+            seg_list.append(batch_label[j, ...])
+            segp_list.append(batch_output[j, ...])
+            center_list.append(batch_center_pred[j, :])
+            heading_cls_list.append(batch_hclass_pred[j])
+            heading_res_list.append(batch_hres_pred[j])
+            size_cls_list.append(batch_sclass_pred[j])
+            size_res_list.append(batch_sres_pred[j, :])
+            rot_angle_list.append(batch_rot_angle[j])
+            score_list.append(batch_scores[j])
+            onehot_list.append(batch_one_hot_vec[i])
+        
+    '''
+    return np.argmax(logits, 2), centers, heading_cls, heading_res, \
+        size_cls, size_res, scores
+
+	batch_output, batch_center_pred, \
+        batch_hclass_pred, batch_hres_pred, \
+        batch_sclass_pred, batch_sres_pred, batch_scores = \
+            inference(sess, ops, batch_data,
+                batch_one_hot_vec, batch_size=batch_size)
+    '''
+    if FLAGS.dump_result:
+        print('dumping...')
+        with open(output_filename, 'wp') as fp:
+            pickle.dump(ps_list, fp)
+            pickle.dump(seg_list, fp)
+            pickle.dump(segp_list, fp)
+            pickle.dump(center_list, fp)
+            pickle.dump(heading_cls_list, fp)
+            pickle.dump(heading_res_list, fp)
+            pickle.dump(size_cls_list, fp)
+            pickle.dump(size_res_list, fp)
+            pickle.dump(rot_angle_list, fp)
+            pickle.dump(score_list, fp)
+            pickle.dump(onehot_list, fp)
+
+    # Write detection results for KITTI evaluation
+    print('Number of point clouds: %d' % (len(ps_list)))
+    write_detection_results(result_dir, TEST_DATASET.id_list,
+                            TEST_DATASET.type_list, TEST_DATASET.box2d_list,
+                            center_list, heading_cls_list, heading_res_list,
+                            size_cls_list, size_res_list, rot_angle_list, score_list)
+    # Make sure for each frame (no matter if we have measurment for that frame),
+    # there is a TXT file
+    output_dir = os.path.join(result_dir, 'data')
+    if FLAGS.idx_path is not None:
+        to_fill_filename_list = [line.rstrip() + '.txt' \
+                                 for line in open(FLAGS.idx_path)]
+        fill_files(output_dir, to_fill_filename_list)
+
+    if FLAGS.return_all_loss:
+        return test_total_loss / test_n_samples, \
+               test_iou2d / test_n_samples, \
+               test_iou3d / test_n_samples, \
+               test_acc / test_n_samples, \
+               test_iou3d_acc / test_n_samples, \
+               test_mask_loss / test_n_samples, \
+               test_center_loss / test_n_samples, \
+               test_heading_class_loss / test_n_samples, \
+               test_size_class_loss / test_n_samples, \
+               test_heading_residuals_normalized_loss / test_n_samples, \
+               test_size_residuals_normalized_loss / test_n_samples, \
+               test_stage1_center_loss / test_n_samples, \
+               test_corners_loss / test_n_samples
+    else:
+        return test_total_loss / test_n_samples, \
+               test_iou2d / test_n_samples, \
+               test_iou3d / test_n_samples, \
+               test_acc / test_n_samples, \
+               test_iou3d_acc / test_n_samples
+
 
 if __name__=='__main__':
     '''
@@ -442,5 +693,29 @@ if __name__=='__main__':
     test box estimation accuracy (IoU=0.7): 0.777397
 
     train/kitti_eval/evaluate_object_3d_offline dataset/KITTI/object/training/label_2/ train/kitti_caronly_v1
+    
+    Thank you for participating in our evaluation!
+    Loading detections...
+    number of files for evaluation: 3769
+      done.
+    save train/kitti_caronly_v1/plot/car_detection.txt
+    car_detection AP: 100.000000 100.000000 100.000000
+    PDFCROP 1.38, 2012/11/02 - Copyright (c) 2002-2012 by Heiko Oberdiek.
+    ==> 1 page written on `car_detection.pdf'.
+    Finished 2D bounding box eval.
+    Going to eval ground for class: car
+    save train/kitti_caronly_v1/plot/car_detection_ground.txt
+    car_detection_ground AP: 91.310730 84.289154 77.309975
+    PDFCROP 1.38, 2012/11/02 - Copyright (c) 2002-2012 by Heiko Oberdiek.
+    ==> 1 page written on `car_detection_ground.pdf'.
+    Finished Birdeye eval.
+    Going to eval 3D box for class: car
+    save train/kitti_caronly_v1/plot/car_detection_3d.txt
+    car_detection_3d AP: 73.388466 67.669502 69.552841
+    PDFCROP 1.38, 2012/11/02 - Copyright (c) 2002-2012 by Heiko Oberdiek.
+    ==> 1 page written on `car_detection_3d.pdf'.
+    Finished 3D bounding box eval.
+    Your evaluation results are available at:
+    train/kitti_caronly_v1
 
     '''
