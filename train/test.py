a6e8bbd43d0cb0b8ca9734359203a4debb04af7d
simon3dv <gzfansiming@gmail.com>
2020-01-30 13:02:07 +0800

    update train and test

--- train/test.py
+++ train/test.py
@@ -574,7 +574,7 @@ def test_from_rgb_detection(model, loader):
             size_res_list.append(batch_sres_pred[j, :])
             rot_angle_list.append(batch_rot_angle[j])
             score_list.append(batch_scores[j])
-            onehot_list.append(batch_one_hot_vec[i])
+            onehot_list.append(batch_one_hot_vec[j])
 
     '''
     return np.argmax(logits, 2), centers, heading_cls, heading_res, \
