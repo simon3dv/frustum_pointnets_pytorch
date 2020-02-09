35c7a36e377117c86f74cc532b27e17b9ee8e044
simon3dv <gzfansiming@gmail.com>
2020-02-09 21:16:14 +0800

    test

--- train/test.py
+++ train/test.py
@@ -390,15 +390,21 @@ def test_one_epoch(model, loader):
 
     print('Average pos ratio: %f' % (pos_cnt / float(all_cnt)))
     print('Average npoints: %f' % (float(all_cnt) / len(ps_list)))
-    mean_info /= len(ps_list)
+    mean_info = mean_info / len(ps_list) / FLAGS.num_point
     print('Mean points: x%f y%f z%f' % (mean_info[0,0],mean_info[0,1],mean_info[0,2]))
     print('Max points: x%f y%f z%f' % (max_info[0,0],max_info[0,1],max_info[0,2]))
     print('Min points: x%f y%f z%f' % (min_info[0,0],min_info[0,1],min_info[0,2]))
     '''
+    2020.2.9
     nuScenes:        
     Mean points: x-67.062391 y865.505454 z36125.768758                         
     Max points: x3.142300 y1.966641 z99.342896                               
     Min points: x0.000000 y0.000000 z0.000000
+    kitti:
+    Mean points: x26.538174 y1010.627422 z25555.257181
+    Max points: x1.805200 y1.805186 z74.910660
+    Min points: x0.000000 y0.000000 z0.000000
+
     '''
     if FLAGS.return_all_loss:
         return test_total_loss / test_n_samples, \
