f93b96153dc2a3277e9b23d5781a39984ce6f8da
simon3dv <gzfansiming@gmail.com>
2020-02-20 23:47:51 +0800

    update model_util

--- models/model_util.py
+++ models/model_util.py
@@ -32,10 +32,17 @@ g_type_mean_size = {'Car': np.array([3.88311640418,1.62856739989,1.52563191462])
                     'Tram': np.array([16.17150617,2.53246914,3.53079012]),
                     'Misc': np.array([3.64300781,1.54298177,1.92320313])}
 '''
+'''
 #trainval
 g_type_mean_size = {'Car': np.array([3.88311640418,1.62856739989,1.52563191462]),
                     'Pedestrian': np.array([0.84422524,0.66068622,1.76255119]),
                     'Cyclist': np.array([1.76282397,0.59706367,1.73698127])}
+'''
+#train_kitti
+g_type_mean_size = {'Car': np.array([3.88311640418,1.62856739989,1.52563191462]),
+                    'Pedestrian': np.array([0.84422524,0.66068622,1.76255119]),
+                    'Cyclist': np.array([1.76282397,0.59706367,1.73698127])}
+
 g_mean_size_arr = np.zeros((NUM_SIZE_CLUSTER, 3)) # size clustrs
 for i in range(NUM_SIZE_CLUSTER):
     g_mean_size_arr[i,:] = g_type_mean_size[g_class2type[i]]
