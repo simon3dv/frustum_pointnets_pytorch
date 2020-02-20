c683d742a66d7dfa08b30242f29cf76cfec1abd0
simon3dv <gzfansiming@gmail.com>
2020-02-20 23:43:36 +0800

    update kitti_info

--- models/model_util.py
+++ models/model_util.py
@@ -14,12 +14,15 @@ import torch.nn.functional as F
 # -----------------
 
 NUM_HEADING_BIN = 12
-NUM_SIZE_CLUSTER = 8 # one cluster for each type
+NUM_SIZE_CLUSTER = 3 #8 # one cluster for each type
 NUM_OBJECT_POINT = 512
-g_type2class={'Car':0, 'Van':1, 'Truck':2, 'Pedestrian':3,
-              'Person_sitting':4, 'Cyclist':5, 'Tram':6, 'Misc':7}
+
+#g_type2class={'Car':0, 'Van':1, 'Truck':2, 'Pedestrian':3,
+#              'Person_sitting':4, 'Cyclist':5, 'Tram':6, 'Misc':7}
+g_type2class={'Car':0,'Pedestrian':1,'Cyclist':2}
 g_class2type = {g_type2class[t]:t for t in g_type2class}
 g_type2onehotclass = {'Car': 0, 'Pedestrian': 1, 'Cyclist': 2}
+'''
 g_type_mean_size = {'Car': np.array([3.88311640418,1.62856739989,1.52563191462]),
                     'Van': np.array([5.06763659,1.9007158,2.20532825]),
                     'Truck': np.array([10.13586957,2.58549199,3.2520595]),
@@ -28,6 +31,11 @@ g_type_mean_size = {'Car': np.array([3.88311640418,1.62856739989,1.52563191462])
                     'Cyclist': np.array([1.76282397,0.59706367,1.73698127]),
                     'Tram': np.array([16.17150617,2.53246914,3.53079012]),
                     'Misc': np.array([3.64300781,1.54298177,1.92320313])}
+'''
+#trainval
+g_type_mean_size = {'Car': np.array([3.88311640418,1.62856739989,1.52563191462]),
+                    'Pedestrian': np.array([0.84422524,0.66068622,1.76255119]),
+                    'Cyclist': np.array([1.76282397,0.59706367,1.73698127])}
 g_mean_size_arr = np.zeros((NUM_SIZE_CLUSTER, 3)) # size clustrs
 for i in range(NUM_SIZE_CLUSTER):
     g_mean_size_arr[i,:] = g_type_mean_size[g_class2type[i]]
